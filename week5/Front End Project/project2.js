4c0f1707fcc396898fb40116fccc44492361684b
carlo carbonell <carlo1678@hotmail.com>
2021-04-12 17:13:41 -0400

    after class commits

---
+++ week5/Front End Project/project2.js
@@ -0,0 +1,18 @@
+const grabInput = document.querySelector(".searchButton")
+const userInput = grabInput.nodeValue
+grabInput.addEventListener("click", () => {apiPull()})
+
+
+const apiPull = async (userInput) => {
+    let response = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${userInput}&api-key=5BRNX9snHaZ283VWQa3Cui2EWgzQqo5G`) 
+    let json = await response.json()
+    console.log(json.response.docs)
+    return json
+}
+
+
+
+
+
+
+
---
+++ week5/day5/anagrams.js
@@ -0,0 +1,9 @@
+const isAnagram = (string1, string2) => {
+    if (string1.length === string2.length) {
+    return true }
+    else { 
+    return false
+    }
+}
+
+console.log(isAnagram("hellosss", "carlosss"))
