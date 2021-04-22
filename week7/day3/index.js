9d087a26501b844722a8bd084a6e4b2628b12d65
carlo carbonell <carlo1678@hotmail.com>
2021-04-22 11:14:49 -0400

    first commits for class

--- week7/day3/index.js
+++ week7/day3/index.js
@@ -1,52 +1,97 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const address = "127.0.0.1"
-const PORT = process.env.PORT || 3006
-const pool = require("./db.js")
-console.log(PORT)
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3006;
+const pool = require("./db.js");
 
-// middleware
-app.use(express.json())
-app.use(cors())
+//middleware
+app.use(express.json());
+app.use(cors());
 
-app.get("/", (req,res) => {
-    const message = "Hello, here's the / page!"
-    res.send(message)
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to node server");
+});
 
-// routes that we're defining for our node server
-
-//CRUD 
+// routes that we define for our node server
 
 // create todo
-app.post("/todo", async (req,res) => {
-    try {
-        const {description} = req.body
-        const newTodoInDB = await pool.query("INSERT INTO todo (description) VALUES($1)", [description])
-        console.log(req.body)
-        res.json(newTodoInDB)
-    } catch(err) {
-        console.log(err.message)
-    }
-})
+app.post("/todo", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodoInDB = await pool.query(
+      "INSERT INTO todo (description) VALUES($1)",
+      [description]
+    );
+
+    res.json(newTodoInDB);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 // read the todo list
-app.get("/read_todos", async (req,res) => {
+
+app.get("/read_todos", async (req, res) => {
+  try {
+    const readTodosFromDB = await pool.query("SELECT * from todo");
+    res.json(readTodosFromDB.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// read a specific todo
+app.get("/read_todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params; // this looks at the url for whatever you put in :
+
+    const readSingleTodoFromDB = await pool.query(
+      "SELECT * from todo WHERE todo_id = ($1)",
+      [id]
+    );
+    res.json(readSingleTodoFromDB);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+// update the todo item on todolist
+
+app.put("/update_todos/:id", async (req,res) => {
     try {
-        const readTodosFromDB = await pool.query("SELECT * from todo ")
-        res.json(readTodosFromDB)
-    } catch(err) {
-        console.log(err.message)
+    const { id } = req.params
+    const {description} = req.body
+    
+    const updateToDoinDB = await pool.query(
+        "UPDATE todo SET description = $1 WHERE todo_id = $2",
+        [description, id])
+        res.json("Updated the todos yay!") }
+    catch (err) { 
+
+        console.error(err)
+        
     }
+    
+}   
+   
+)
+
+// delete the todo
+
+app.delete("/delete_todos/:id", async (req,res) => {
+
+    try {
+        const { id } = req.params
+        const deleteToDoInDB = await pool.query(
+        "DELETE FROM todo WHERE todo_id = $1",
+        [id]
+        )
+        res.json("Todo was successfully deleted!")
+        }
+    catch (err) {
+        console.error(err)
+    }
+    
 })
-// update the todo list on todolist
 
-// delete the todo 
-
-
-
-
-
-app.listen(PORT, () => {
-    console.log(`server is on ${PORT}`)
-})
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
